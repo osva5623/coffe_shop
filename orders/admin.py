012c26f1c9d48906aa5266c5a261242b53f2ed72
oswaldo jimenez <oswaldo.jimenez5623@gmail.com>
2024-12-12 10:59:58 -0600

    creacion de ordenes

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderProduct
+
+
+class OrderProductInlineAdmin(admin.TabularInline):
+    model = OrderProduct  # Register your models here.
+    extra=0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    model=Order
+    inlines=[
+        OrderProductInlineAdmin
+    ]
+
+admin.site.register(Order,OrderAdmin)
